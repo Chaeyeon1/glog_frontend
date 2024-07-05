c10fd5cf1b3ab91fe383d4cd681a477b7c124830
chaeyeon1 <66813821+Chaeyeon1@users.noreply.github.com>
2024-07-05 20:19:39 +0900

    feat : add image 확장자

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export const metadata: Metadata = {
     title: 'GLOG',
     description:
       '개발자들을 위해 만들어진 블로그를 작성해보세요! 발자국 시스템, PR 시스템으로 블로그를 꾸준히 쓸 수 있도록 도와줍니다',
-    images: '/assets/GLOG_LOGO',
+    images: '/assets/GLOG_LOGO.png',
   },
   verification: {
     google: 'Ot9R1wLIt3DP15IpoikrTHTptAcVxD5f-MX5poEjgrQ',
