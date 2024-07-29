c1d617473e0a0a96fd0efdcc5a7c65830c3f1efc
chaeyeon1 <slide10245@naver.com>
2024-07-30 01:23:33 +0900

    feat : header에 계정 탈퇴 제거

--- src/components/Header/SettingMenu.tsx
+++ src/components/Header/SettingMenu.tsx
@@ -1,48 +1,22 @@
 'use client';
 
 import { ModalType, TokenType } from '@/types/common';
-import { Dialog, DialogActions, DialogContent, Menu, MenuItem, useTheme } from '@mui/material';
+import { Menu, MenuItem, useTheme } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import PageLink from '../PageLink/PageLink';
 import FriendModal from '../Layout/HeaderFriendModal/FriendModal';
 import { enqueueSnackbar } from 'notistack';
-import { deleteUserApi } from '@/api/userDetail-api';
-import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { useRouter } from 'next/navigation';
-import Button from '../Button/Button';
-import useModalOpen from '@/hooks/useModalOpen';
 
 function SettingMenu({ open, onClose, anchorEl }: ModalType & { anchorEl: null | HTMLElement }) {
   const [friendOpen, setFriendOpen] = useState<boolean>(false);
   const [token, setToken] = useState<TokenType>(null);
   const theme = useTheme();
-  const queryClient = useQueryClient();
-  const {
-    open: dialogOpen,
-    handleClose: handleDialogClose,
-    handleOpen: handleDialogOpen,
-  } = useModalOpen();
-  const router = useRouter();
 
   useEffect(() => {
     if (typeof window !== 'undefined') {
       setToken(localStorage.getItem('token'));
     }
   }, [open]);
-  const { mutateAsync: deleteUserMutate } = useMutation(deleteUserApi, {
-    onSuccess: () => {
-      queryClient.invalidateQueries(['userDetail']);
-      enqueueSnackbar({
-        message: '회원탈퇴가 성공적으로 완료되었습니다.',
-        variant: 'success',
-      });
-      localStorage.removeItem('token');
-      handleDialogClose();
-      router.push('/login');
-      onClose();
-    },
-  });
-
   return (
     <Menu anchorEl={anchorEl} open={open} onClose={onClose}>
       {token && (
@@ -88,14 +62,6 @@ function SettingMenu({ open, onClose, anchorEl }: ModalType & { anchorEl: null |
           <MenuItem>Logout</MenuItem>
         </PageLink>
       )}
-      {token && (
-        <MenuItem
-          onClick={() => {
-            handleDialogOpen();
-          }}>
-          계정탈퇴
-        </MenuItem>
-      )}
       {!token && (
         <MenuItem>
           <PageLink href="/login" onClick={onClose}>
@@ -104,18 +70,6 @@ function SettingMenu({ open, onClose, anchorEl }: ModalType & { anchorEl: null |
         </MenuItem>
       )}
       <FriendModal open={friendOpen} onClose={() => setFriendOpen(false)}></FriendModal>
-      <Dialog open={dialogOpen} onClose={handleDialogClose}>
-        <DialogContent>회원 탈퇴를 진행하시겠습니까?</DialogContent>
-        <DialogActions>
-          <Button onClick={handleDialogClose}>아니요</Button>
-          <Button
-            onClick={() => {
-              deleteUserMutate({ token });
-            }}>
-            네
-          </Button>
-        </DialogActions>
-      </Dialog>
     </Menu>
   );
 }
