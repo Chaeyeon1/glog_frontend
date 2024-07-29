565568f35fc6ef63d2414eba49228ed2e9c7d902
chaeyeon1 <slide10245@naver.com>
2024-07-29 11:30:14 +0900

    feat : 메인 대시보드 collect로 변경

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -10,7 +10,7 @@ export default function NotFound() {
       <Image alt="404 그림" src={'/assets/error-404.png'} width={100} height={100} />
       <h1 color="#000">찾을 수 없는 페이지에요</h1>
       <PageLink
-        href="/"
+        href="/collect"
         style={{ border: '1px solid #444', padding: '4px 8px', borderRadius: '8px' }}
         color="#444">
         메인 대시보드로 돌아가기
