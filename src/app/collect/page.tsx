288418738807f8711b8423492f0c1d3227406b0e
chaeyeon1 <slide10245@naver.com>
2024-08-24 14:03:31 +0900

    feat : 검색 / 모아보기 페이지 분리하기

--- src/app/collect/page.tsx
+++ src/app/collect/page.tsx
@@ -1,45 +1,48 @@
 'use client';
 
 import {
+  ImageList,
   MenuItem,
   outlinedInputClasses,
   Select,
   Stack,
   svgIconClasses,
-  TextField,
   useTheme,
 } from '@mui/material';
 import React, { useEffect, useState } from 'react';
-import { useIsSearchSSR } from '../../../hooks/useRecoilSSR';
-import CollectPosts from './CollectPosts';
-import { CollectFilter } from './_related/types';
-import { useDebounceValue } from 'usehooks-ts';
+import { ViewType } from './_related/types';
+import CollectPost from './CollectPost';
+import { useGetCollectDataQuery } from '@/api/collect-api';
+import { useInView } from 'react-intersection-observer';
 
 function Collect() {
-  const [isSearch] = useIsSearchSSR();
-  const [searchType, setSearchType] = useState<CollectFilter>('user');
   const theme = useTheme();
-  const [searchText, setSearchText] = useState<string>('');
-  const [debouncedValue] = useDebounceValue(searchText, 500);
+  const [ref, inView] = useInView({ threshold: 0, delay: 0 });
+  const [searchType, setSearchType] = useState<ViewType>('likes');
+  const {
+    data: collectData,
+    isFetching,
+    hasNextPage,
+    fetchNextPage,
+  } = useGetCollectDataQuery({ searchType });
+  const [kindArray, setKindArray] = useState(collectData);
 
-  const viewMenuItemList = [
+  const viewMenuItemList: { value: ViewType; label: string }[] = [
     { value: 'likes', label: '좋아요순' },
     { value: 'recent', label: '최신순' },
     { value: 'views', label: '조회수순' },
   ];
-
-  const searchMenuItemList = [
-    { value: 'title', label: '제목' },
-    { value: 'user', label: '작성자' },
-    { value: 'hashtag', label: '해시태그' },
-    { value: 'content', label: '글 내용' },
-  ];
-  const menuItemList = isSearch ? searchMenuItemList : viewMenuItemList;
+  const menuItemList = viewMenuItemList;
 
   useEffect(() => {
-    setSearchType(isSearch ? 'title' : 'recent');
-    setSearchText('');
-  }, [isSearch]);
+    if (inView) {
+      !isFetching && hasNextPage && fetchNextPage();
+    }
+  }, [inView, isFetching, hasNextPage, fetchNextPage]);
+
+  useEffect(() => {
+    setKindArray(collectData);
+  }, [collectData]);
 
   return (
     <>
@@ -71,35 +74,18 @@ function Collect() {
               <MenuItem
                 key={item.value}
                 value={item.value}
-                onClick={() => setSearchType(item.value as CollectFilter)}>
+                onClick={() => setSearchType(item.value)}>
                 {item.label}
               </MenuItem>
             ))}
           </Select>
-          {isSearch && (
-            <TextField
-              size="small"
-              placeholder="검색어를 입력해주세요"
-              value={searchText}
-              onChange={(event) => setSearchText(event.target.value)}
-              sx={{
-                width: '50%',
-                [`&& .${outlinedInputClasses.root}`]: {
-                  [`& .${outlinedInputClasses.notchedOutline}`]: {
-                    border: '1px solid transparent',
-                  },
-                },
-                ':hover': {
-                  [`&.${outlinedInputClasses.notchedOutline}`]: {
-                    border: '1px solid transparent',
-                  },
-                },
-                [`& .${svgIconClasses.root}`]: { fill: theme.palette.primary.main },
-              }}
-            />
-          )}
         </Stack>
-        <CollectPosts searchType={searchType} searchText={debouncedValue} />
+        <ImageList variant="masonry" cols={4} gap={4}>
+          {kindArray?.map((item) => {
+            return <CollectPost item={item} key={item.postId} />;
+          })}
+          <div ref={ref} />
+        </ImageList>
       </Stack>
     </>
   );
